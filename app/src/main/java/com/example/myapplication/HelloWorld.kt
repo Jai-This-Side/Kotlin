5fd793caf4a8cbaa1ceaf781e486c95c7691f256
Jai-This-Side <jaisinghprsnl99@gmail.com>
2025-04-05 21:07:07 +0530

    Kotlin Day 1

---
+++ app/src/main/java/com/example/myapplication/HelloWorld.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+public class HelloWorld{
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>){
+            println("Hello World");
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Variables.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+public class Variables {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>){
+            Int
+            String
+            Boolean
+            Float
+            Double
+            Long
+            Char
+            Byte
+            Short
+
+            var num1 : Int = 10
+
+            println("Hello World $num1, Welcome!!") // concatenation and lambda function
+            
+
+
+        }
+    }
+}
